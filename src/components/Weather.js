d216fce54e12659a9fd55e38ba394651b4a1e23f
EURosales <eduardo_rosales26@outlook.com>
2022-01-07 11:05:51 -0600

    initial commit

---
+++ src/components/Weather.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import DisplayWeather from "./DisplayWeather";
+import "./weather.css";
+
+function Weather() {
+  const [weather, setWeather] = useState([]);
+  const [form, setForm] = useState({
+    city: "",
+    country: "",
+  });
+
+  const APIKEY = "08a3dbcbac739964faab9809c318b886";
+
+  async function weatherData(e) {
+    e.preventDefault();
+
+    //fetch data given a city
+    if (form.city == "") {
+      alert("Please add at least a city name");
+    } else {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+
+      setWeather({ data: data });
+    }
+  }
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name == "city") {
+      setForm({ ...form, city: value });
+    }
+    if (name == "country") {
+      setForm({ ...form, country: value });
+    }
+  };
+
+  return (
+    <div className="weather">
+      <span className="title">Weather App</span>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={(e) => handleChange(e)}
+        />
+        
+        <input
+          type="text"
+          placeholder="Country"
+          name="country"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="getweather" onClick={(e) => weatherData(e)}>
+          Check weather
+        </button>
+      </form>
+
+      {weather.data != undefined ? (
+        <div>
+          <DisplayWeather data={weather.data} />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Weather;
